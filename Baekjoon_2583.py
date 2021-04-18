a6c50aec52b47c6e1c938bb6d2f4c7e660cdfd53
HangyulChoi <45627019+HangyulChoi@users.noreply.github.com>
2021-04-19 02:24:27 +0900

    Update Baekjoon_2583.py

--- Baekjoon_2583.py
+++ Baekjoon_2583.py
@@ -16,4 +16,68 @@ M, N, K는 모두 100 이하의 자연수이다.
 첫째 줄에 분리되어 나누어지는 영역의 개수를 출력한다. 
 둘째 줄에는 각 영역의 넓이를 오름차순으로 정렬하여 빈칸을 사이에 두고 출력한다.
 """
-s
+import sys
+sys.setrecursionlimit(10000)
+m, n, k = map(int, input().split())
+s = [[0]*n for _ in range(m)]
+dy = [-1, 1, 0, 0]; dx  =  [0, 0, -1, 1]
+cnt = []
+for i in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    for j in range(y1, y2):
+        for k in range(x1, x2):
+            s[j][k] = 1
+for i in range(m):
+    for j in range(n):
+        if s[i][j] == 0:
+            count = 1
+            s[i][j] = 1
+            q = [[i, j]]
+            while q:
+                x, y = q[0][0], q[0][1]
+                del q[0]
+                for k in range(4):
+                    x1 = x + dx[k]
+                    y1 = y + dy[k]
+                    if 0 <= x1 < m and 0 <= y1 < n and s[x1][y1] == 0:
+                        s[x1][y1] = 1
+                        count += 1
+                        q.append([x1, y1])
+            cnt.append(count)
+print(len(cnt))
+cnt.sort()
+print(*cnt)
+
+#%%
+# dfs
+import sys
+sys.setrecursionlimit(10000)
+m, n, k = map(int, input().split())
+s = [[0]*m for _ in range(n)]
+dx = [-1, 1, 0, 0]; dy = [0, 0, -1, 1]
+ans = []
+count = 0
+def dfs(q, p):
+    global count
+    s[q][p] = 1
+    count += 1
+    for t in range(4):
+        nx = p + dx[t]; ny = q + dy[t]
+        if (0<=nx<m and 0<=ny<n and s[ny][nx]==0):
+            dfs(ny, nx)
+
+for i in range(k):
+    y1, x1, y2, x2 = map(int, input().split())
+    for j in range(y1, y2):
+        for k in range(x1, x2):
+            s[j][k] = -1
+            
+for i in range(n):
+    for j in range(m):
+        if s[i][j] == 0:
+            count = 0
+            dfs(i, j)
+            ans.append(count)
+print(len(ans))
+ans.sort()
+print(*ans)
